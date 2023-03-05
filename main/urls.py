c6b15c80fa945b8c842272054810fd64bed2d023
uikola <ugulaev806@yandex.com>
2023-03-05 12:47:36 +0400

    Adding User Profile modification functionality

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, other_page, BbLoginView, profile, BbLogoutView
+from .views import index, other_page, BbLoginView, profile, BbLogoutView, ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/logout/', BbLogoutView.as_view(), name='logout'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BbLoginView.as_view(), name='login'),
